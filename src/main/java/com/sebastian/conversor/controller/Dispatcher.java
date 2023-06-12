a02899b80671a8b92062403364ced4aed851d357
Sebastian Robles Camacho <sebastian.rob.cam@gmail.com>
2023-06-12 01:49:11 -0600

    feat: Create dispatcher for conversion logic
    Add a method in the dispatcher that receives two values, 'thing' and 'option'. 'thing' is used in an if block within the dispatcher to determine which class should be called, while 'option' will be used within a method in the class to execute the conversion logic.
    
    #dispatcher #conversion

---
+++ src/main/java/com/sebastian/conversor/controller/Dispatcher.java
@@ -0,0 +1,27 @@
+package main.java.com.sebastian.conversor.controller;
+
+import main.java.com.sebastian.conversor.model.ConCurrency;
+import main.java.com.sebastian.conversor.model.ConMeasures;
+import main.java.com.sebastian.conversor.model.ConTemperature;
+
+public class Dispatcher {
+
+    public Dispatcher() {
+    }
+
+    public void todo(String thing, String option) {
+
+        if(thing.equals("currency")) {
+            ConCurrency currency = new ConCurrency();
+        } else if (thing.equals("measures")) {
+            ConMeasures measures = new ConMeasures();
+            System.out.println("convert measures");
+        } else if (thing.equals("temperature")) {
+            ConTemperature temperature = new ConTemperature();
+            System.out.println("convert temperature");
+        } else {
+            System.out.println("invalid value");
+        }
+
+    }
+}
