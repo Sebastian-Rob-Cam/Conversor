f1e7dcabd117a9a68de0ef670b7f51a9ab9470c1
Sebastian Robles Camacho <sebastian.rob.cam@gmail.com>
2023-06-12 19:02:35 -0600

    refactor: Modify connectionAPI to return JSON as string
    Update the connectionAPI to retrieve JSON data and return it as a string format, ensuring compatibility and ease of use in the application.
    
    #connectionAPI #JSON #string

--- src/main/java/com/sebastian/conversor/model/connectionAPI.java
+++ src/main/java/com/sebastian/conversor/model/connectionAPI.java
@@ -1,7 +1,6 @@
 package main.java.com.sebastian.conversor.model;
 
-import com.google.gson.*;
-
+import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -14,32 +13,34 @@ public class connectionAPI {
     public String getJSON(String currencyCode) {
         String urlString = "https://v6.exchangerate-api.com/v6/a1f3f9d4e4ed91862cf61d73/latest/" + currencyCode;
 
-        String jsonToStringFormatted = null;
+        String json = null;
         try {
             URL url = new URL(urlString);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.connect();
 
-            int responceCode = connection.getResponseCode();
-            if (responceCode != 200) {
-                throw new RuntimeException("Ocurrio un error " + responceCode);
+            int responseCode = connection.getResponseCode();
+            if (responseCode != 200) {
+                throw new RuntimeException("Ocurrió un error " + responseCode);
             } else {
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-                JsonParser jp = new JsonParser();
-                JsonElement root = jp.parse(new InputStreamReader((InputStream) connection.getContent()));
-                JsonObject jsonobj = root.getAsJsonObject();
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                reader.close();
 
-                // String req_result = jsonobj.get("result").getAsString();
-
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                jsonToStringFormatted = gson.toJson(jsonobj);
+                json = stringBuilder.toString();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-
-         return jsonToStringFormatted;
+        return json;
     }
+
 }
